cf7dd868b5961768805150f9683245aebaa0f648
Rodrigo <rodrigo@gigmit.com>
2022-08-31 12:35:57 +0200

    Add keywords to MusicEvent

--- lib/schema_dot_org/music_event.rb
+++ lib/schema_dot_org/music_event.rb
@@ -8,15 +8,18 @@ module SchemaDotOrg
   # Model the Schema.org `MusicEvent`.  See http://schema.org/MusicEvent
   class MusicEvent < Thing
     attr_accessor :start_date,
-                  :location
+                  :location,
+                  :keywords
 
     validates :start_date, type: Date, presence: true
     validates :location, type: SchemaDotOrg::PostalAddress, presence: true
+    validates :keywords, type: String, allow_nil: true
 
     def _to_json_struct
       super.merge(
         'startDate' => start_date.to_s,
-        'location' => location.to_json_struct
+        'location' => location.to_json_struct,
+        'keywords' => keywords
       )
     end
   end
