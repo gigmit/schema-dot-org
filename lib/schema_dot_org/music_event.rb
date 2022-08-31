3cb8d2969f7668fd15683b048013d87c1c7bd157
Rodrigo <rodrigo@gigmit.com>
2022-08-31 11:04:30 +0200

    Add location to MusicEvent

--- lib/schema_dot_org/music_event.rb
+++ lib/schema_dot_org/music_event.rb
@@ -2,17 +2,21 @@
 
 require 'schema_dot_org'
 require 'schema_dot_org/thing'
+require 'schema_dot_org/postal_address'
 
 module SchemaDotOrg
   # Model the Schema.org `MusicEvent`.  See http://schema.org/MusicEvent
   class MusicEvent < Thing
-    attr_accessor :start_date
+    attr_accessor :start_date,
+                  :location
 
     validates :start_date, type: Date, presence: true
+    validates :location, type: SchemaDotOrg::PostalAddress, presence: true
 
     def _to_json_struct
       super.merge(
-        'startDate' => start_date.to_s
+        'startDate' => start_date.to_s,
+        'location' => location.to_json_struct
       )
     end
   end
