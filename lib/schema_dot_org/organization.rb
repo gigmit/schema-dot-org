515cd8232ed32af2f06449c0c17ac3ce79391b24
Rodrigo <rodrigo@gigmit.com>
2022-08-31 10:12:42 +0200

    Fix Organization address to be of type PostalAddress

--- lib/schema_dot_org/organization.rb
+++ lib/schema_dot_org/organization.rb
@@ -5,6 +5,7 @@ require 'schema_dot_org'
 require 'schema_dot_org/thing'
 require 'schema_dot_org/person'
 require 'schema_dot_org/place'
+require 'schema_dot_org/postal_address'
 
 module SchemaDotOrg
   class Organization < Thing
@@ -21,7 +22,7 @@ module SchemaDotOrg
     validates :founding_date, type: Date, allow_nil: true
     validates :founding_location, type: SchemaDotOrg::Place, allow_nil: true
     validates :logo, type: String, allow_nil: true
-    validates :address, type: String, allow_nil: true
+    validates :address, type: SchemaDotOrg::PostalAddress, allow_nil: true
     validates :keywords, type: String, allow_nil: true
 
     def _to_json_struct
@@ -30,7 +31,7 @@ module SchemaDotOrg
         'founder' => founder&.to_json_struct,
         'foundingDate' => founding_date&.to_s,
         'foundingLocation' => founding_location&.to_json_struct,
-        'address' => address,
+        'address' => address&.to_json_struct,
         'keywords' => keywords
       )
     end
